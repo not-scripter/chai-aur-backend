ab466ee3033431a9688c8b9af21be4e2dd415909
MRINMOY MONDAL <mm2922894@gmail.com>
2024-09-15 00:57:36 +0530

    data modeling part 1

---
+++ data-modeling/models/user.model.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "uswrname is required"],
+      unique: [true, "username should be unique"],
+      lowercase: [true, "username should be in lowercase"],
+    },
+    email: {
+      type: String,
+      required: [true, "email is required"],
+      unique: [true, "email should be unique"],
+      lowercase: [true, "email should be in lowercase"],
+    },
+    password: {
+      type: String,
+      required: [true, "password is required"],
+    },
+  },
+  { timestamps: true },
+);
+
+export const User = mongoose.model("User", userSchema);
+
+//NOTE: MongoDB will automatically convert "User" to "users" it is their standard practice
